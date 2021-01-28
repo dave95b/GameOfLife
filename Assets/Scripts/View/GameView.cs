f86d2d091e722046bb170cb0de0becf756c9afed
Dave <dave95.b@gmail.com>
2021-01-28 20:31:01 +0100

    Dynamic GameView

--- Assets/Scripts/View/GameView.cs
+++ Assets/Scripts/View/GameView.cs
@@ -11,11 +11,45 @@ namespace GameOfLive.View
         private RawImage[] images;
 
         [SerializeField]
+        private RawImage prefab;
+
+        [SerializeField]
         private GameViewModel viewModel;
+
+        [SerializeField]
+        GridLayoutGroup grid;
 
         private void Awake()
         {
+            viewModel.OnGameCreated += CreateView;
             viewModel.OnGameUpdated += UpdateView;
+        }
+
+        private void CreateView(GameState gameState)
+        {
+            UpdateGridProperties(gameState);
+            CreateCells(gameState);
+
+            UpdateView(gameState);
+        }
+
+        private void UpdateGridProperties(GameState gameState)
+        {
+            float gridSize = CalculateGridSize(gameState);
+
+            grid.cellSize = new Vector2(gridSize, gridSize);
+            grid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            grid.constraintCount = gameState.Width;
+        }
+
+        private void CreateCells(GameState gameState)
+        {
+            Transform parent = grid.transform;
+            int count = gameState.Width * gameState.Height;
+            images = new RawImage[count];
+
+            for (int i = 0; i < count; i++)
+                images[i] = Instantiate(prefab, parent);
         }
 
         private void UpdateView(GameState gameState)
@@ -30,6 +64,19 @@ namespace GameOfLive.View
                     image.enabled = state;
                 }
             }
+        }
+
+        private static float CalculateGridSize(GameState gameState)
+        {
+            var camera = Camera.main;
+            float camHeight = camera.orthographicSize * 2;
+            float camWidth = camera.aspect * camHeight;
+
+            const int sizeFactor = 128;
+            float maxHeight = sizeFactor * camHeight;
+            float maxWidth = sizeFactor * camWidth;
+            float gridSize = Mathf.Min(maxHeight, maxWidth) / gameState.Height;
+            return gridSize;
         }
 
         [ContextMenu("Find images")]
